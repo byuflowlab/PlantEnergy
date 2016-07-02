012fdbf432f09e80e5fae75c461d0b30c3f71feb
Jared J. Thomas <jaredthomas68@gmail.com>
2016-07-02 16:52:52 -0600

    moved contents of florisse to wakeexchange and began editing to create wakeexchange

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from numpy.distutils.core import setup, Extension
-
-module1 = Extension('_floris', sources=['src/florisse/floris.f90', 'src/florisse/adStack.c', 'src/florisse/adBuffer.f'],
-                   extra_compile_args=['-O2', '-c'])
-
-module2 = Extension('_florisDiscontinuous', sources=['src/florisse/florisDiscontinuous.f90', 'src/florisse/adStack.c', 'src/florisse/adBuffer.f'],
-                   extra_compile_args=['-O2', '-c'])
+from numpy.distutils.core import setup
 
 setup(
-    name='FLORISSE',
-    version='0.0.0',
-    description='differentiable floris wake model with cosine factor',
-    install_requires=['openmdao>=1.5','akima>=1.0.0'],
+    name='WakeExchange',
+    version='0.0.1',
+    description='Wind farm optimization interface allowing wake models to be switched out',
+    install_requires=['openmdao>=1.6.3','FLORISSE'],
     package_dir={'': 'src'},
-    ext_modules=[module1, module2],
-    dependency_links=['https://github.com/andrewning/akima/tarball/master#egg=akima'],
-    packages=['florisse'],
+    dependency_links=['http://github.com/OpenMDAO/OpenMDAO.git@master', 'https://github.com/WISDEM/FLORISSE.git@develop'],
+    packages=['wakeexchange'],
     license='Apache License, Version 2.0',
 )
