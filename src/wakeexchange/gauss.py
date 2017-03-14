6b911bbab6444c2df7e2af442f76df37b74960fe
Jared Thomas <jaredthomas68@gmail.com>
2017-03-14 02:33:44 -0600

    made changes for adding bastankhah yaw model and example

--- src/wakeexchange/gauss.py
+++ src/wakeexchange/gauss.py
@@ -10,11 +10,19 @@ from gaussianwake.gaussianwake import GaussianWake
 
 def add_gauss_params_IndepVarComps(openmdao_object):
 
-    openmdao_object.add('p0', IndepVarComp('model_params:ke', 0.052, pass_by_object=True))
-    openmdao_object.add('p1', IndepVarComp('model_params:rotation_offset_angle', val=1.56, units='deg',
+    openmdao_object.add('bp0', IndepVarComp('model_params:ke', 0.052, pass_by_object=True))
+    openmdao_object.add('bp1', IndepVarComp('model_params:rotation_offset_angle', val=1.56, units='deg',
                                            pass_by_object=True))
-    openmdao_object.add('p2', IndepVarComp('model_params:spread_angle', val=5.84, units='deg', pass_by_object=True))
+    openmdao_object.add('bp2', IndepVarComp('model_params:spread_angle', val=5.84, units='deg', pass_by_object=True))
 
+    # params for Bastankhah model with yaw
+    # openmdao_object.add('bp3', IndepVarComp('model_params:ky', 0.07, pass_by_object=True))
+    # openmdao_object.add('bp4', IndepVarComp('model_params:kz', 0.07, pass_by_object=True))
+    # openmdao_object.add('bp5', IndepVarComp('model_params:alpha', 2.32, pass_by_object=True))
+    # openmdao_object.add('bp6', IndepVarComp('model_params:beta', 0.154, pass_by_object=True))
+    # openmdao_object.add('bp7', IndepVarComp('model_params:I', 0.01, pass_by_object=True))
+    # openmdao_object.add('bp8', IndepVarComp('model_params:yaw_mode', val='bastankhah', pass_by_object=True))
+    # openmdao_object.add('bp9', IndepVarComp('model_params:spread_mode', val='bastankhah', pass_by_object=True))
 
 class gauss_wrapper(Group):
 
