corrected circle boundary calculations and gradients. completed circle boundary gradient test

src/wakeexchange/GeneralWindFarmComponents.py
@@ -595,22 +595,21 @@ class BoundaryComp(Component):
         self.nTurbines = nTurbines
         self.nVertices = nVertices
         if nVertices > 1:
-            self.type = 'polygon'
+            self.type = type = 'polygon'
         elif nVertices == 1:
-            self.type = 'circle'
+            self.type = type = 'circle'
         else:
             ValueError('nVertices in BoundaryComp must be greater than 0')
 
-        if type is 'polygon':
-            # Explicitly size input arrays
+        if type == 'polygon':
+            #     Explicitly size input arrays
             self.add_param('boundaryVertices', np.zeros([nVertices, 2]), units='m', pass_by_obj=True,
                            desc="vertices of the convex hull CCW in order s.t. boundaryVertices[i] -> first point of face"
                                 "for unit_normals[i]")
             self.add_param('boundaryNormals', np.zeros([nVertices, 2]), pass_by_obj=True,
                            desc="unit normal vector for each boundary face CCW where boundaryVertices[i] is "
                                 "the first point of the corresponding face")
-        elif type is 'circle':
-            self.nVertices = 1
+        elif type == 'circle':
             self.add_param('boundary_radius', val=1000., units='m', pass_by_obj=True, desc='radius of wind farm boundary')
             self.add_param('boundary_center', val=np.array([0., 0.]), units='m', pass_by_obj=True,
                            desc='x and y positions of circular wind farm boundary center')
@@ -633,7 +632,7 @@ class BoundaryComp(Component):
         turbineX = params['turbineX']
         turbineY = params['turbineY']
 
-        if self.type is 'polygon':
+        if self.type == 'polygon':
             # put locations in correct arrangement for calculations
             locations = np.zeros([self.nTurbines, 2])
             for i in range(0, self.nTurbines):
@@ -645,11 +644,11 @@ class BoundaryComp(Component):
             unknowns['boundaryDistances'] = calculate_distance(locations,
                                                                params['boundaryVertices'], params['boundaryNormals'])
 
-        elif self.type is 'circle':
+        elif self.type == 'circle':
             xc = params['boundary_center'][0]
             yc = params['boundary_center'][1]
             r = params['boundary_radius']
-            unknowns['boundaryDistances'] = np.sqrt(np.power((turbineX - xc), 2) - np.power((turbineY - yc), 2)) - r
+            unknowns['boundaryDistances'] = r - np.sqrt(np.power((turbineX - xc), 2) + np.power((turbineY - yc), 2))
 
         else:
             ValueError('Invalid value (%s) encountered in BoundaryComp input -type-. Must be one of [polygon, circle]'
@@ -657,9 +656,9 @@ class BoundaryComp(Component):
 
     def linearize(self, params, unknowns, resids):
 
-        unit_normals = params['boundaryNormals']
+        if self.type == 'polygon':
+            unit_normals = params['boundaryNormals']
 
-        if self.type is 'polygon':
             # initialize array to hold distances from each point to each face
             dfaceDistance_dx = np.zeros([self.nTurbines*self.nVertices, self.nTurbines])
             dfaceDistance_dy = np.zeros([self.nTurbines*self.nVertices, self.nTurbines])
@@ -680,17 +679,19 @@ class BoundaryComp(Component):
                     dfaceDistance_dx[i*self.nVertices+j, i] = np.vdot(ddistanceVec_dx, unit_normals[j])
                     dfaceDistance_dy[i*self.nVertices+j, i] = np.vdot(ddistanceVec_dy, unit_normals[j])
 
-        elif self.type is 'circle':
+        elif self.type == 'circle':
             turbineX = params['turbineX']
             turbineY = params['turbineY']
             xc = params['boundary_center'][0]
             yc = params['boundary_center'][1]
             r = params['boundary_radius']
 
-            common_term = 0.5*np.power((np.power((turbineX - xc), 2) - np.power((turbineY - yc), 2)), -0.5)
-            dfaceDistance_dx = common_term*2.*(turbineX - xc)
-            dfaceDistance_dy = common_term*2.*(turbineY - yc)
-
+            common_term = -0.5*np.power((np.power((turbineX - xc), 2) + np.power((turbineY - yc), 2)), -0.5)
+            A = np.eye(self.nTurbines, self.nTurbines)
+            B = common_term * 2. * (turbineX - xc)
+            dfaceDistance_dx = A*B
+            B = common_term * 2. * (turbineY - yc)
+            dfaceDistance_dy = A*B
         else:
             ValueError('Invalid value (%s) encountered in BoundaryComp input -type-. Must be one of [polygon, circle]'
                        % (type))

test/test_gradients.py
@@ -45,7 +45,7 @@ class TotalDerivTestsFlorisAEPOpt(unittest.TestCase):
         yaw = np.random.rand(nTurbines)*60. - 30.
 
         # Define flow properties
-        nDirections = 50
+        nDirections = 4
         windSpeeds = np.random.rand(nDirections)*20        # m/s
         air_density = 1.1716    # kg/m^3
         windDirections = np.random.rand(nDirections)*360.0
@@ -747,13 +747,13 @@ class GradientTestsPower(unittest.TestCase):
         np.testing.assert_allclose(self.J['all_directions.direction_group0.powerComp'][('dir_power0', 'Cp')]['J_fwd'], self.J['all_directions.direction_group0.powerComp'][('dir_power0', 'Cp')]['J_fd'], self.rtol, self.atol)
         np.testing.assert_allclose(self.J['all_directions.direction_group0.powerComp'][('dir_power0', 'rotorDiameter')]['J_fwd'], self.J['all_directions.direction_group0.powerComp'][('dir_power0', 'rotorDiameter')]['J_fd'], self.rtol, self.atol)
 
-class GradientTestsGeneralComponents(unittest.TestCase):
+class GradientTestsConstraintComponents(unittest.TestCase):
 
     def setUp(self):
 
         nTurbines = 3
         self.rtol = 1E-6
-        self.atol = 1E-6
+        self.atol = 1E-3
 
         np.random.seed(seed=10)
 
@@ -784,7 +784,7 @@ class GradientTestsGeneralComponents(unittest.TestCase):
         yaw = np.random.rand(nTurbines)*60. - 30.
 
         # Define flow properties
-        nDirections = 50
+        nDirections = 5
         windSpeeds = np.random.rand(nDirections)*20        # m/s
         air_density = 1.1716    # kg/m^3
         windDirections = np.random.rand(nDirections)*360.0
@@ -847,12 +847,10 @@ class GradientTestsGeneralComponents(unittest.TestCase):
 
             # provide values for hull constraint
             if prob is prob_polygon:
-                count += 1
-                print(count)
                 prob['boundaryVertices'] = boundaryVertices
                 prob['boundaryNormals'] = boundaryNormals
             elif prob is prob_circle:
-                prob['boundary_center'] = np.array(boundary_center_x, boundary_center_y)
+                prob['boundary_center'] = np.array([boundary_center_x, boundary_center_y])
                 prob['boundary_radius'] = boundary_radius
 
             # run problem
@@ -868,18 +866,18 @@ class GradientTestsGeneralComponents(unittest.TestCase):
 
     def testSpacingCon(self):
 
-        np.testing.assert_allclose(self.J[('sc', 'turbineX')]['rel error'], self.J[('sc', 'turbineX')]['rel error'], self.rtol, self.atol)
-        np.testing.assert_allclose(self.J[('sc', 'turbineY')]['rel error'], self.J[('sc', 'turbineY')]['rel error'], self.rtol, self.atol)
+        np.testing.assert_allclose(self.J[('sc', 'turbineX')]['J_fwd'], self.J[('sc', 'turbineX')]['J_fd'], self.rtol, self.atol)
+        np.testing.assert_allclose(self.J[('sc', 'turbineY')]['J_fwd'], self.J[('sc', 'turbineY')]['J_fd'], self.rtol, self.atol)
 
     def testBoundaryConPolygon(self):
 
-        np.testing.assert_allclose(self.J[('boundaryDistances', 'turbineX')]['rel error'], self.J[('boundaryDistances', 'turbineX')]['rel error'], self.rtol, self.atol)
-        np.testing.assert_allclose(self.J[('boundaryDistances', 'turbineY')]['rel error'], self.J[('boundaryDistances', 'turbineY')]['rel error'], self.rtol, self.atol)
+        np.testing.assert_allclose(self.J[('boundaryDistances', 'turbineX')]['J_fwd'], self.J[('boundaryDistances', 'turbineX')]['J_fd'], self.rtol, self.atol)
+        np.testing.assert_allclose(self.J[('boundaryDistances', 'turbineY')]['J_fwd'], self.J[('boundaryDistances', 'turbineY')]['J_fd'], self.rtol, self.atol)
 
     def testBoundaryConCircle(self):
 
-        np.testing.assert_allclose(self.J_circle[('boundaryDistances', 'turbineX')]['rel error'], self.J_circle[('boundaryDistances', 'turbineX')]['rel error'], self.rtol, self.atol)
-        np.testing.assert_allclose(self.J_circle[('boundaryDistances', 'turbineY')]['rel error'], self.J_circle[('boundaryDistances', 'turbineY')]['rel error'], self.rtol, self.atol)
+        np.testing.assert_allclose(self.J_circle[('boundaryDistances', 'turbineX')]['J_fwd'], self.J_circle[('boundaryDistances', 'turbineX')]['J_fd'], self.rtol, self.atol)
+        np.testing.assert_allclose(self.J_circle[('boundaryDistances', 'turbineY')]['J_fwd'], self.J_circle[('boundaryDistances', 'turbineY')]['J_fd'], self.rtol, self.atol)
 
 
 # TODO create gradient tests for all components