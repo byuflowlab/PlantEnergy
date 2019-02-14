f444da5904adceb80de4cbf3ec7518b9291da923
spencer.mcomber <spencer.mcomber@gmail.com>
2019-02-14 07:23:27 -0700

    Unifying relaxation factor name to wec_factor so that all wake models use the same name for their relaxation factors.

--- src/plantenergy/floris.py
+++ src/plantenergy/floris.py
@@ -128,7 +128,7 @@ def add_floris_params_IndepVarComps(openmdao_object, use_rotor_components=False)
                         promotes=['*'])
 
     # ###############    Wake Expansion Continuation (WEC) ##############
-    openmdao_object.add('fp20', IndepVarComp('model_params:WECRelaxationFactor', val=1.0, pass_by_obj=True,
+    openmdao_object.add('fp20', IndepVarComp('model_params:wec_factor', val=1.0, pass_by_obj=True,
                                              desc='relaxation factor as defined in Thomas 2018. doi:10.1088/1742-6596/1037/4/042012'), promotes=['*'])
 
 
@@ -192,7 +192,7 @@ class FLORISParameters(Component):
         self.add_param('model_params:z_ref', 90., units='m', pass_by_obj=True, desc='height at which wind_speed is measured')
         self.add_param('model_params:z0', 0., units='m', pass_by_obj=True, desc='ground height')
         # ###############    Wake Expansion Continuation (WEC) ##############
-        self.add_param('model_params:WECRelaxationFactor', val=1.0, pass_by_obj=True, desc='relaxation factor as defined in Thomas 2018. doi:10.1088/1742-6596/1037/4/042012')
+        self.add_param('model_params:wec_factor', val=1.0, pass_by_obj=True, desc='relaxation factor as defined in Thomas 2018. doi:10.1088/1742-6596/1037/4/042012')
 
         # add corresponding unknowns
         self.add_output('floris_params:kd', 0.15 if not use_rotor_components else 0.17, pass_by_obj=True,
@@ -250,7 +250,7 @@ class FLORISParameters(Component):
                                 desc='override all parameters and use FLORIS as original in first Wind Energy paper')
 
         # ###############    Wake Expansion Continuation (WEC) ##############
-        self.add_output('floris_params:WECRelaxationFactor', val=1.0, pass_by_obj=True,
+        self.add_output('floris_params:wec_factor', val=1.0, pass_by_obj=True,
                          desc='relaxation factor as defined in Thomas 2018. doi:10.1088/1742-6596/1037/4/042012')
 
     def solve_nonlinear(self, params, unknowns, resids):
