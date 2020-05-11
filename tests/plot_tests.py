583eed82580d5e82bcee82f8ee391f31ee0fb474
jaredthomas68 <jaredthomas68@gmail.com>
2020-05-11 16:21:05 -0600

    working on power curve

--- tests/plot_tests.py
+++ tests/plot_tests.py
@@ -19,11 +19,21 @@ def plot_power_curve_with_spline_on_cut_in_speed():
     rotor_diameter = 80.
     air_density = 1.225
     Ar = 0.25 * np.pi * rotor_diameter ** 2
-    cp_curve_cp = power_data[:, 1] * (1E6) / (0.5 * air_density * power_data[:, 0] ** 3 * Ar)
+    cp_curve_cp = power_data[:, 1] * (1E6) / (0.5 * air_density*Ar*power_data[:, 0] ** 3)
     cp_curve_v = power_data[:, 0]
 
-    print(cp_curve_cp)
+    # power_data = np.loadtxt("./input_files/v80powercurve.csv", delimiter=",")
+    # print(power_data)
+    # rotor_diameter = 80.
+    # air_density = 1.225
+    # Ar = 0.25 * np.pi * rotor_diameter ** 2
+    # cp_curve_cp = power_data[:, 1] * (1E3) / (0.5 * air_density * Ar * power_data[:, 0] ** 3)
+    # cp_curve_v = power_data[:, 0]
 
+    print(cp_curve_cp)
+    power_data = np.loadtxt("./input_files/NREL5MWCPCT.txt")
+    cp_curve_cp = power_data[:, 1]
+    cp_curve_v = power_data[:, 0]
     # define openmdao problem
     prob = om.Problem()
     prob.model.add_subsystem('power', WindDirectionPower(nTurbines=nSpeeds, cp_points=cp_curve_cp.size), promotes=['*'])
@@ -43,17 +53,17 @@ def plot_power_curve_with_spline_on_cut_in_speed():
     ratedws = 16.
     ratedp = 2E3
 
-    # cutin = 3.  # m/s
-    # cutout = 25.  # m/s
-    # ratedws = 11.4  # m/s
-    # ratedp = 5000.  # kW
+    cutin = 3.  # m/s
+    cutout = 25.  # m/s
+    ratedws = 11.4  # m/s
+    ratedp = 5000.  # kW
 
     # assign values to turbine states
     prob['cut_in_speed'] = np.ones(nSpeeds) * cutin
     prob['cut_out_speed'] = np.ones(nSpeeds) * cutout
     prob['rated_power'] = np.ones(nSpeeds) * ratedp
     prob['rated_wind_speed'] = np.ones(nSpeeds) * ratedws
-    prob['use_power_curve_definition'] = True
+    prob['use_power_curve_definition'] = False
 
     # run the problem
     prob.run_model()
@@ -84,14 +94,29 @@ def plot_power_curve_with_spline_on_cut_in_speed():
     data = np.loadtxt('./input_files/niayifar_vestas_v80_power_curve_observed.txt', delimiter=",")
 
     scaler = 1E-3
-    plt.plot(velocities, prob_power*scaler, 'r', label='PlantEnergy')
+    plt.plot(velocities, prob_power*scaler, 'k', label='Model')
     # plt.plot(cp_curve_v, cp_curve_cp, 'b:')
-    plt.plot(velocities, func_power*scaler, 'k--')
-    plt.plot(data[:, 0], data[:, 1], label='Data')
+    plt.plot(velocities, func_power*scaler, 'r--', label="function")
+    plt.plot(data[:, 0], data[:, 1], '--k', label='Data')
+    plt.plot(data[:, 0], data[:, 1], '--k', label='Data')
     plt.legend(frameon=False)
-    plt.ylabel('MW')
-    plt.xlabel('m/s')
+    plt.ylabel('Wind Turbine Power (MW)')
+    plt.xlabel('Inflow Velocity (m/s)')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    # plt.tick_params(top='none', right='none')
+    plt.tight_layout()
+    plt.savefig('v80power.pdf', transparent=True)
+
     plt.show()
 
 if __name__ == "__main__":
+
+
+    font = {'size': 15}
+    plt.rc('font', **font)
+
+    plt.rcParams['xtick.major.pad'] = '8'
+    plt.rcParams['ytick.major.pad'] = '8'
     plot_power_curve_with_spline_on_cut_in_speed()
